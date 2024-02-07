50284e67a2ac1ff4a03273260cfe1b1b384a414d
OPTANWAR2001 <optanwar10@gmail.com>
2024-02-08 02:18:54 +0530

    new todo list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,7 +6,7 @@ const TodoList = () => {
     <div className="flex justify-center items-center h-full my-3 ">
       <div className="bg-blue-950 border-sky-500 border-8 py-10 px-8 text-center items-center w-3/5 rounded-md">
         <h1 className="text-white text-3xl font-bold ">
-          <span className="border-b-2 pb-0.5">Grocery Shopping</span>
+          <span className="border-b-2 pb-0.5">Todo List App</span>
         </h1>
         <TextFile />
       </div>
